207a5a0f442b77e61fb5c75cac76f34811252a82
Streetmeat25 <kotpersikchannel@gmail.com>
2023-10-09 15:34:03 +0300

    change iterate buttons  placement in code + added iteration

--- src/components/DishItem.jsx
+++ src/components/DishItem.jsx
@@ -7,13 +7,10 @@ import {Button, IconButton, Modal} from "@mui/material";
 import classes from "./modals/dishModal/DishModal.module.css";
 import {useDispatch, useSelector} from "react-redux";
 
-const DishItem = ({dish, dishNumber}) => {
+const DishItem = ({dish}) => {
     const [modal, setModal] = useState(false)
     const navigate = useNavigate();
 
-    const dishes = useSelector(state => {
-        return state.dishReducer.dishes
-    })
 
     return (
         <div className="dish-card">
@@ -22,45 +19,6 @@ const DishItem = ({dish, dishNumber}) => {
                      style={{cursor: 'pointer', background: `#d6d6d6  url(${process.env.PUBLIC_URL + dish?.imgPath})`}}>
                 </div>
             </Link>
-            <div className="dish-buttons">
-                {/*<IconButton onClick={()=>console.log("clcik")}>*/}
-                {/*    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-
-                {/*        <rect width="50" height="50" rx="24.8718" fill="white"/>*/}
-                {/*        <path d="M45.8718 47.3974L49.8718 43.8974L45.8718 40.3974" transform="translate(-23,-19)"*/}
-                {/*              stroke="#595959" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-
-                {/*    </svg>*/}
-                {/*</IconButton>*/}
-                <PreviousButton>
-                    <IconButton onClick={()=>dish = dishes[Math.floor(Math.random() * dishes?.length)]}>
-                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                        <rect width="50" height="50" rx="24.8718" fill="#F5F5F5"/>
-                        <path d="M49.8718 40.3974L45.8718 43.8974L49.8718 47.3974" transform="translate(-24,-19)"
-                              stroke="#595959" stroke-width="2"
-                              stroke-linecap="round" stroke-linejoin="round"/>
-
-                    </svg>
-                    </IconButton>
-                </PreviousButton>
-                <NextButton>
-                    <IconButton onClick={()=> {
-                                dish = dishes[Math.floor(Math.random() * dishes?.length)];
-                                console.log(dish);
-                                }}
-                    >
-                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                            <rect width="50" height="50" rx="24.8718" fill="white"/>
-                            <path d="M45.8718 47.3974L49.8718 43.8974L45.8718 40.3974" transform="translate(-23,-19)"
-                                  stroke="#595959" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-
-                        </svg>
-                    </IconButton>
-
-                </NextButton>
-            </div>
 
             <div className="dish-card-info">
                 <p className="dish-name">{dish?.name} </p>
--- src/components/DishList.jsx
+++ src/components/DishList.jsx
@@ -1,16 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import DishItem from "./DishItem";
 import UpdateButton from "./UI/buttons/UpdateButton/UpdateButton";
+import {IconButton} from "@mui/material";
 
 const DishList = ({dishes}) => {
-    const [randomDishNumber, setRandomDishNumber] = useState(Math.floor(Math.random() * dishes?.length))
+    const [dishNumber, setDishNumber] = useState(0)
     return (
         <div className="dish-list">
-            <DishItem dish={dishes[randomDishNumber]} dishNumber="FirstDish"/>
-            {/*<DishItem dish={dishes[Math.floor(Math.random() * dishes.length)]} dishNumber="SecondDish"/>*/}
-            {/*<DishItem dish={dishes[Math.floor(Math.random() * dishes.length)]} dishNumber="Drink"/>*/}
-            <UpdateButton onClick={() => setRandomDishNumber(Math.floor(Math.random() * dishes?.length) )}>
-                Обновить
+            <DishItem dish={dishes[dishNumber]}/>
+            <IconButton
+                onClick={()=> {
+                    if(dishNumber == 0) return
+                    setDishNumber(i => i-1);
+                }}
+                style={{position: "absolute", top: "30%", left: "-6%", background: "transparent"}}>
+                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                    <rect width="50" height="50" rx="24.8718" fill="#F5F5F5"/>
+                    <path d="M49.8718 40.3974L45.8718 43.8974L49.8718 47.3974" transform="translate(-24,-19)"
+                          stroke="#595959" stroke-width="2"
+                          stroke-linecap="round" stroke-linejoin="round"/>
+
+                </svg>
+            </IconButton>
+            <IconButton
+                onClick={()=> {
+                    if(dishNumber == dishes.length - 1) return
+                    setDishNumber(i => i+1);
+                }}
+                style={{position: "absolute", top: "30%", left: "94%"}}>
+                <svg width="50" height="50" viewBox="0 0 50 50" fill="transparent" xmlns="http://www.w3.org/2000/svg">
+
+                    <rect width="50" height="50" rx="24.8718" fill="white"/>
+                    <path d="M45.8718 47.3974L49.8718 43.8974L45.8718 40.3974" transform="translate(-23,-19)"
+                          stroke="#595959" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+
+                </svg>
+            </IconButton>
+
+            <UpdateButton>
+                + Добавить
             </UpdateButton>
         </div>
     );
